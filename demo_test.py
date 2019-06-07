4ad1baba968079395169b8e3bcf6708afdcb036b
itskhagendra <itskhagendra@hotmail.com>
2019-06-07 17:40:11 +0530

    Test Updated
    Test File Updated

--- demo_test.py
+++ demo_test.py
@@ -1,21 +1,25 @@
 import pickle
 import numpy as np
+import tensorflow as tf
 from keras.models import load_model
 from keras.preprocessing.sequence import pad_sequences
 
-model = load_model("small_dataset_model.h5")
-print("Model Loaded Successfully")
+tf.logging.set_verbosity(tf.logging.ERROR)
 
-with open('tokenizer.pickle', 'rb') as handle:
+print("Loading Ai Engine ....")
+model = load_model("./Models/big_dataset_model.h5")
+print("Engine Loaded Successfully")
+
+print("Loading Support Dependencies ....")
+with open('./Models/big_dataset_token.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
-print("Tokenizer loaded successfully")
+print("Dependencies loaded successfully")
 
 txt = input("Enter Your Thoughts ")
 twt = [txt]
 
 twt = tokenizer.texts_to_sequences(twt)
-print(twt)
-twt = pad_sequences(twt, maxlen=28, dtype='int32', value=0)
+twt = pad_sequences(twt, maxlen=40, dtype='int32', value=0)
 sentiment = model.predict(twt, batch_size=1, verbose=0)[0]
 print(sentiment)
 
